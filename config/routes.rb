2d5f09d4c6d9d5854eba9dad2a4ca601352e527a
VitBur <vitaly.burshtein@cloudinary.com>
2025-02-27 10:43:10 +0200

    Changing action name, and using before_action and rescue_from

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'thumbnail', to: 'thumbnails#generate'
+  get 'thumbnail', to: 'thumbnails#thumbnail_create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
