939421b263419c939622f30d67965f2224f3359d
VitBur <vitaly.burshtein@cloudinary.com>
2025-03-05 16:04:58 +0200

    deleting minitest files

--- test/controllers/thumbnails_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ThumbnailsControllerTest < ActionDispatch::IntegrationTest
-  test "should get generate" do
-    get thumbnails_generate_url
-    assert_response :success
-  end
-
-end
